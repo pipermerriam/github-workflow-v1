61026778bf5c654c7e7764a4db892b6b5e011db2
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-07-26 16:09:31 -0600

    working on lint errors

--- src/intOrdinal.js
+++ src/intOrdinal.js
@@ -9,21 +9,21 @@ function intOrginal(value) {
   if ((value % 100 >= 11) && (value % 100 <= 20)) {
     Ordinal = value + "th";
     return Ordinal;
-  };
+  } ;
   value = value % 10;
   switch (value) {
-    case 1:
-      Ordinal = value + "st";
-      break;
-    case 2:
-      Ordinal = value + "nd";
-      break;
-    case 3:
-      Ordinal = value + "rd";
-      break;
-    default:
-      Ordinal = value + "th";
-      break;
+  case 1:
+    Ordinal = value + "st";
+    break;
+  case 2:
+    Ordinal = value + "nd";
+    break;
+  case 3:
+    Ordinal = value + "rd";
+    break;
+  default:
+    Ordinal = value + "th";
+    break;
   };
   console.log("Ordinal value is ",Ordinal);
   return Ordinal;
