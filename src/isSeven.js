d108d8e4aaffde80a90081e26a09ba6c1074f604
annahuffman147 <annahuffman.plc@gmail.com>
2016-07-26 14:41:26 -0600

    linting test passing

--- src/isSeven.js
+++ src/isSeven.js
@@ -1,23 +1,23 @@
-function isSeven(value) {
+function isPrime(value) {
   "use strict";
-  /*
-   *  Returns boolean as to whether the provided value is the number 7.
-   */
-
-  if (value < 10 && value > 5 && isPrime(value)){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isPrime(value){
-  for (var i = 2; i < value; i++) {
+  var i;
+  for (i = 2; i < value; i += 1) {
     if (value % i === 0) {
       return false;
     }
   }
   return value > 1;
+}
+
+function isSeven(value) {
+  "use strict";
+  /*
+   *  Returns boolean as to whether the provided value is the number 7.
+   */
+  if (value < 10 && value > 5 && isPrime(value)) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isSeven;
