16e94aaf2a53d1f1d2315a0c41e5884a0a1fe9f9
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-07-26 15:37:01 -0600

    removed spaces that were causing lint errors

--- src/intOrdinal.js
+++ src/intOrdinal.js
@@ -6,33 +6,24 @@ function intOrginal(value) {
 
 
   var Ordinal;
-  if((value%100 >= 11) && (value%100 <=20)) {
+  if((value % 100 >= 11) && (value % 100 <= 20)) {
     Ordinal = value + "th"
     return Ordinal;
   }
-  value = value%10;
+  value = value % 10;
   switch (value) {
-
-      case 1:
-
-          Ordinal = value +  "st";
-          break;
-
-      case 2:
-
-          Ordinal = value + "nd";
-          break;
-
-      case 3:
-
-            Ordinal = value + "rd";
-            break;
-
-      default:
-
-              Ordinal = value + "th";
-              break;
-
+    case 1:
+      Ordinal = value +  "st";
+      break;
+    case 2:
+      Ordinal = value + "nd";
+      break;
+    case 3:
+      Ordinal = value + "rd";
+      break;
+    default:
+      Ordinal = value + "th";
+      break;
   }
   console.log("Ordinal value is ",Ordinal);
   return Ordinal;
