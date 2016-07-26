52558cdb8b5119aeafa5f45d816f68f2742320c4
annahuffman147 <annahuffman.plc@gmail.com>
2016-07-26 14:03:25 -0600

    implements isSeven function

--- src/isSeven.js
+++ src/isSeven.js
@@ -3,8 +3,21 @@ function isSeven(value) {
   /*
    *  Returns boolean as to whether the provided value is the number 7.
    */
-  /*jslint unparam: true*/
-  return true;
+
+  if (value < 10 && value > 5 && isPrime(value)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isPrime(value){
+  for (var i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return value > 1;
 }
 
 module.exports = isSeven;
