7d5aeda9646bd039cee5187e4eb3a646b6580544
Lou Fontana <loufontana@gmail.com>
2016-07-26 14:26:02 -0600

    Added hundreds

--- spec/intWord_spec.js
+++ spec/intWord_spec.js
@@ -4,4 +4,8 @@ describe("CQL.intWord", function() {
   it("Converts 1000 to 1 thousand", function() {
     expect(CQL.intWord(1000)).toBe("1 thousand");
   });
+  it("Converts 100 to 1 hundred", function(){
+    expect(CQL.intWord(100)).toBe("1 hundred");
+  });
+
 });
--- src/intWord.js
+++ src/intWord.js
@@ -3,12 +3,16 @@ function intWord(value) {
   /*
    *  Converts an integer to a friendly text representation
    */
+   var units = "";
    value = value.toString();
 
    var firstDigit = value.slice(0, 1);
    var appendWith = value.slice(1);
    if (appendWith.length === 3) {
-     var units = "thousand";
+     units = "thousand";
+   }
+   if (appendWith.length === 2) {
+     units = "hundred";
    }
 
    return firstDigit + " " + units;
