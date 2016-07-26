aedcd8316439b361546998883643ceba47cb8118
Lou Fontana <loufontana@gmail.com>
2016-07-26 14:55:11 -0600

    fixed multiple linting issues

--- src/intWord.js
+++ src/intWord.js
@@ -3,19 +3,18 @@ function intWord(value) {
   /*
    *  Converts an integer to a friendly text representation
    */
-   var units = "";
-   value = value.toString();
+  value = value.toString();
+  var units = "",
+    firstDigit = value.slice(0, 1),
+    appendWith = value.slice(1);
+  if (appendWith.length === 3) {
+    units = "thousand";
+  }
+  if (appendWith.length === 2) {
+    units = "hundred";
+  }
 
-   var firstDigit = value.slice(0, 1);
-   var appendWith = value.slice(1);
-   if (appendWith.length === 3) {
-     units = "thousand";
-   }
-   if (appendWith.length === 2) {
-     units = "hundred";
-   }
-
-   return firstDigit + " " + units;
+  return firstDigit + " " + units;
 }
 
 module.exports = intWord;
