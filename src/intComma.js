1fd13cf8725d7cfd3ba47534deba3fa19ed4a8c7
annahuffman147 <annahuffman.plc@gmail.com>
2016-07-26 14:48:53 -0600

    linting tests pass

--- src/intComma.js
+++ src/intComma.js
@@ -3,16 +3,17 @@ function intComma(value) {
   /*
    *  Converts an integer to a string with commas every 3 digits.
    */
-  var stringValue = value.toString()
-  var segments = []
+  var stringValue = value.toString(),
+    segments = [];
+
   while (stringValue.length > 2) {
-    segments.push(stringValue.substring(stringValue.length-3))
-    stringValue = stringValue.substring(0, stringValue.length-3)
+    segments.push(stringValue.substring(stringValue.length - 3));
+    stringValue = stringValue.substring(0, stringValue.length - 3);
   }
-  segments.push(stringValue)
+  segments.push(stringValue);
   return segments.reverse()
     .join(',')
-    .replace(/^,/, '')
+    .replace(/^,/, '');
 }
 
 module.exports = intComma;
